490198d696d7e67579b5c58c5588273bc3076a38
juzt1n <JUSTINTOH03.20@ichat.sp.edu.sg>
2021-07-28 13:08:59 +0800

    NEW

--- Controller/app.js
+++ Controller/app.js
@@ -126,7 +126,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5432;
 app.listen(port, function () {
     console.log('App is listening to port ' + port);
 });
