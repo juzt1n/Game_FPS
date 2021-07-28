81cb922747184404b9b9d9c678a32e3933534f95
juzt1n <JUSTINTOH03.20@ichat.sp.edu.sg>
2021-07-28 14:19:05 +0800

    package change

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const game = require('../game');
+const game = require('./game');
 
 
 
